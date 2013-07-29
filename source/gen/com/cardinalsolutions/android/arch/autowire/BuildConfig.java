1632d6058e1a1462bcc4bdbd71aa2dd278957099
Jacob Kanipe-Illig <jkanipe-illig@cardinalsolutions.com>
2013-07-29 13:40:36 -0400

    Add Jar and separate source files.

---
+++ source/gen/com/cardinalsolutions/android/arch/autowire/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cardinalsolutions.android.arch.autowire;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
