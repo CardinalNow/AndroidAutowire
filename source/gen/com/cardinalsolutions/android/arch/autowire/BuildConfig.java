4a652d8bcbd3373d55b1e61d98bd41abb56aaeda
Jacob Kanipe-Illig <jkanipe-illig@cardinalsolutions.com>
2013-09-13 09:42:26 -0400

    Change gitignore to exclude some compiled/generated files

--- source/gen/com/cardinalsolutions/android/arch/autowire/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.cardinalsolutions.android.arch.autowire;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
